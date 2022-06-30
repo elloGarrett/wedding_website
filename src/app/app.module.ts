a8cfab77f9d45c5ac268bc97fa2786cc88a5fa11
Garrett Mason <garrett.m.mason@jbhunt.com>
2022-06-30 08:32:58 -0500

    General layout is complete, but images are still not displaying correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './shared/app-header/app-header.component';
+import { AppNavComponent } from './shared/app-nav/app-nav.component';
+import { AppFooterComponent } from './shared/app-footer/app-footer.component';
+import { HomePageComponent } from './features/home-page/home-page.component';
+import { RsvpComponent } from './features/rsvp/rsvp.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppNavComponent,
+    AppFooterComponent,
+    HomePageComponent,
+    RsvpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomePageComponent},
+      {path: 'rsvp', component: RsvpComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
